689e9fc7293ddf914751b8b8252521378b586906
Nikita Obergan <Obergannikita@gmail.com>
2023-08-23 11:08:26 -0400

    Installl package add apdbcontext class and add connection string

---
+++ 127_WebAPIProductsReviews3/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _127_WebAPIProductsReviews3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- 127_WebAPIProductsReviews3/Program.cs
+++ 127_WebAPIProductsReviews3/Program.cs
@@ -1,4 +1,7 @@
 
+using _127_WebAPIProductsReviews3.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace _127_WebAPIProductsReviews3
 {
     public class Program
@@ -13,6 +16,15 @@ namespace _127_WebAPIProductsReviews3
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+
+            builder.Services.AddDbContext<ApplicationDbContext>(dbContextOptions => dbContextOptions
+                            .UseMySql(connectionString, serverVersion)
+                            .LogTo(Console.WriteLine, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                            .EnableDetailedErrors());
 
             var app = builder.Build();
 
